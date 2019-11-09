b35162e337e3fff608684f6d681182a46dbe0545
Руфат_Ольга <rufattabaev@gmail.com>
2019-11-09 10:31:30 +0300

    fixes #8

--- requests.sql
+++ requests.sql
@@ -2,7 +2,7 @@ CREATE TABLE products (
   id       INTEGER PRIMARY KEY AUTOINCREMENT,
   category TEXT NOT NULL,
   name     TEXT NOT NULL,
-  status   TEXT NOT NULL       DEFAULT 'available', -- else can be 'AVAILABLE', 'DISCONTINUED'
+  status   TEXT NOT NULL       DEFAULT 'AVAILABLE', -- else can be 'AVAILABLE', 'DISCONTINUED'
   actual_price  NOT NULL CHECK (actual_price > 0)
 );
 
@@ -31,7 +31,7 @@ INSERT INTO users (login, name) VALUES ('third user', 'Ivan');
 CREATE TABLE orders (
   id             INTEGER PRIMARY KEY AUTOINCREMENT,
   user_id        INTEGER REFERENCES users,
-  product_status TEXT NOT NULL       DEFAULT 'selected' --else can be 'PAID', 'CANCELED'
+  product_status TEXT NOT NULL       DEFAULT 'SELECTED' --else can be 'PAID', 'CANCELED'
 );
 
 INSERT INTO orders(user_id, product_status) VALUES (1, 'selected');
@@ -41,7 +41,7 @@ INSERT INTO orders(user_id, product_status) VALUES (3, 'paid');
 CREATE TABLE order_status_for_registered_users (
   id           INTEGER PRIMARY KEY AUTOINCREMENT,
   order_id     INTEGER REFERENCES orders,
-  order_status TEXT NOT NULL       DEFAULT 'current' --else can be 'COMPLETED', 'UNREDEEMED'
+  order_status TEXT NOT NULL       DEFAULT 'CURRENT' --else can be 'COMPLETED', 'UNREDEEMED'
 );
 
 INSERT INTO order_status_for_registered_users (order_id, order_status) VALUES (1, 'completed');
